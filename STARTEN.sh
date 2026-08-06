36d92e7c6882ec6489e106d1a04dd56c839020a0
pppoppi <provoware.one@gmail.com>
2026-08-07 00:03:53 +0200

    feat: route standard launcher through persistent debug wrapper

--- STARTEN.sh
+++ STARTEN.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-exec "$ROOT_DIR/videobatch.sh" start
+BOOTSTRAP_PYTHON="${VIDEOBATCH_BOOTSTRAP_PYTHON:-python3}"
+exec "$BOOTSTRAP_PYTHON" "$ROOT_DIR/scripts/debug_launcher.py" "$@"
