857bd8845c129f9c3d9490297d8ffe56b83dd203
pppoppi <provoware.one@gmail.com>
2026-08-06 22:57:12 +0200

    ui: activate compact KPI and safe window mixins

--- src/videobatch_fast/canonical_ui.py
+++ src/videobatch_fast/canonical_ui.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 from tkinter import Tk
 
+from .canonical_kpi_compact_mixin import CanonicalKpiCompactMixin
 from .canonical_kpi_detail_mixin import CanonicalKpiDetailMixin
-from .canonical_shell_chrome import CanonicalShellChromeMixin
-from .canonical_shell_workspace import CanonicalShellWorkspaceMixin
 from .canonical_window_mixin import CanonicalWindowMixin
+from .canonical_shell_workspace import CanonicalShellWorkspaceMixin
+from .canonical_shell_chrome import CanonicalShellChromeMixin
 from .error_handling import error_definition
 from .startup_handshake import signal_ui_ready
 from .ui import VideoBatchFastUI
@@ -13,6 +14,7 @@ from .ui_components import SolutionDialog
 
 
 class CanonicalVideoBatchFastUI(
+    CanonicalKpiCompactMixin,
     CanonicalKpiDetailMixin,
     CanonicalWindowMixin,
     CanonicalShellWorkspaceMixin,
