f52e6ccc8f1642f78ee1d300831f406797e59a46
pppoppi <provoware.one@gmail.com>
2026-09-04 04:52:52 +0200

    39A: remove redundant rollback copy from integration lineage

--- Backup/A32.2_vor_A33/STARTEN.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-BOOTSTRAP_PYTHON="${VIDEOBATCH_BOOTSTRAP_PYTHON:-python3}"
-exec "$BOOTSTRAP_PYTHON" "$ROOT_DIR/scripts/debug_launcher.py" "$@"
